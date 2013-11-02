9043aeac5a27c90b4832990ef65e2561af740254
Jacob Schlitz <jschlitz@gmail.com>
2013-11-02 18:41:17 -0400

    merp.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -9,4 +9,18 @@ angular.module('myApp.controllers', []).
   }])
   .controller('MyCtrl2', [function() {
 
-  }]);
+  }])
+    .controller('MyCtrl3', ['$scope', function($scope){
+      $scope.theList = ['foo', 'bar', 'baz'];
+
+      $scope.removeThing = function (x){
+        console.log(x);
+        $scope.theList.splice($scope.theList.indexOf(x), 1);
+      };
+
+      $scope.addThing = function (x){
+        console.log(x);
+        $scope.theList.push(x);
+
+      };
+}]);
